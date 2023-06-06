f7ec7f6d746f7ada92e80cecd4cc38ab3181ac1b
Geronimo Nicola <geronicola1696@gmail.com>
2023-06-06 17:29:21 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true })); // Para que funcionen los formularios
+
+app.get('/', (req, res) => {
+  console.log(req.body)
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
