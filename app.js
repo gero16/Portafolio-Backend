83c2b59d38bf0d1680d1cacd8e9b4715fae9c5f4
Geronimo Nicola <geronicola1696@gmail.com>
2023-06-06 17:39:10 -0300

    bienvenido 1.1

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true })); // Para que funcionen los formularios
 
-app.get('/', (req, res) => {
-  console.log(req.body)
-  res.json({usuario: "Bienvenido!"})
-})
+
+
+app.use('/', express.static('public'));
 
 app.post('/', (req, res) => {
   console.log(req.body)
